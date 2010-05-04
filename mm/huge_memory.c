9ee20f14f5f5406841ac95cee49423d228ddc43d
Andrea Arcangeli <aarcange@redhat.com>
2010-05-05 01:44:17 +0200

    remove anon-vma changes
    Return to the 2.6.33 code.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -812,14 +812,13 @@ static void __split_huge_page(struct page *page,
 			      struct anon_vma *anon_vma)
 {
 	int mapcount, mapcount2;
-	struct anon_vma_chain *avc;
+	struct vm_area_struct *vma;
 
 	BUG_ON(!PageHead(page));
 	BUG_ON(PageTail(page));
 
 	mapcount = 0;
-	list_for_each_entry(avc, &anon_vma->head, same_anon_vma) {
-		struct vm_area_struct *vma = avc->vma;
+	list_for_each_entry(vma, &anon_vma->head, anon_vma_node) {
 		unsigned long addr = vma_address(page, vma);
 		if (addr == -EFAULT)
 			continue;
@@ -830,8 +829,7 @@ static void __split_huge_page(struct page *page,
 	__split_huge_page_refcount(page);
 
 	mapcount2 = 0;
-	list_for_each_entry(avc, &anon_vma->head, same_anon_vma) {
-		struct vm_area_struct *vma = avc->vma;
+	list_for_each_entry(vma, &anon_vma->head, anon_vma_node) {
 		unsigned long addr = vma_address(page, vma);
 		if (addr == -EFAULT)
 			continue;
