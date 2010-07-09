5b4a7f3aebfe8c90e379ebeb6ba6b661c670576c
Andrea Arcangeli <aarcange@redhat.com>
2010-07-09 17:52:55 +0200

    set VM_FAULT_WRITE in do_swap_page
    Set the flag if do_swap_page is decowing the page the same way do_wp_page would
    too.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/memory.c
+++ mm/memory.c
@@ -2730,6 +2730,7 @@ static int do_swap_page(struct mm_struct *mm, struct vm_area_struct *vma,
 		flags &= ~FAULT_FLAG_WRITE;
 		/* = 2 because the swapcache may be PageAnon already */
 		exclusive = 2;
+		ret |= VM_FAULT_WRITE;
 	}
 	flush_icache_page(vma, page);
 	set_pte_at(mm, address, page_table, pte);
