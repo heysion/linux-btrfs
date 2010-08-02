966990381229730b4f6a6ef87235aafb8991f119
Andrea Arcangeli <aarcange@redhat.com>
2010-08-02 20:41:56 +0200

    freeze khugepaged and ksmd
    It's unclear why schedule friendly kernel threads can't be taken away by the
    CPU through the scheduler itself. It's safer to stop them as they can trigger
    memory allocation, if kswapd also freezes itself to avoid generating I/O they
    have too.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/ksm.c
+++ mm/ksm.c
@@ -33,6 +33,7 @@
 #include <linux/mmu_notifier.h>
 #include <linux/swap.h>
 #include <linux/ksm.h>
+#include <linux/freezer.h>
 
 #include <asm/tlbflush.h>
 #include "internal.h"
@@ -1386,7 +1387,7 @@ static void ksm_do_scan(unsigned int scan_npages)
 	struct rmap_item *rmap_item;
 	struct page *uninitialized_var(page);
 
-	while (scan_npages--) {
+	while (scan_npages-- && likely(!freezing(current))) {
 		cond_resched();
 		rmap_item = scan_get_next_rmap_item(&page);
 		if (!rmap_item)
@@ -1411,6 +1412,8 @@ static int ksm_scan_thread(void *nothing)
 		if (ksmd_should_run())
 			ksm_do_scan(ksm_thread_pages_to_scan);
 		mutex_unlock(&ksm_thread_mutex);
+
+		try_to_freeze();
 
 		if (ksmd_should_run()) {
 			schedule_timeout_interruptible(
@@ -1953,6 +1956,7 @@ static int __init ksm_init(void)
 	struct task_struct *ksm_thread;
 	int err;
 
+	set_freezable();
 	err = ksm_slab_init();
 	if (err)
 		goto out;
