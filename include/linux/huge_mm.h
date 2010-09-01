0c09e26cbcfa23b475212b9fae05c1ca022d7eb5
Andrea Arcangeli <aarcange@redhat.com>
2010-09-01 20:12:35 +0200

    madvise(MADV_HUGEPAGE)
    Add madvise MADV_HUGEPAGE to mark regions that are important to be hugepage
    backed. Return -EINVAL if the vma is not of an anonymous type, or the feature
    isn't built into the kernel. Never silently return success.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Acked-by: Rik van Riel <riel@redhat.com>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -99,6 +99,7 @@ extern void __split_huge_page_pmd(struct mm_struct *mm, pmd_t *pmd);
 #endif
 
 extern unsigned long vma_address(struct page *page, struct vm_area_struct *vma);
+extern int hugepage_madvise(unsigned long *vm_flags);
 static inline int PageTransHuge(struct page *page)
 {
 	VM_BUG_ON(PageTail(page));
@@ -121,6 +122,11 @@ static inline int split_huge_page(struct page *page)
 #define wait_split_huge_page(__anon_vma, __pmd)	\
 	do { } while (0)
 #define PageTransHuge(page) 0
+static inline int hugepage_madvise(unsigned long *vm_flags)
+{
+	BUG_ON(0);
+	return 0;
+}
 #endif /* CONFIG_TRANSPARENT_HUGEPAGE */
 
 #endif /* _LINUX_HUGE_MM_H */
