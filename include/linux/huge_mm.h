8e8b82243f899168ed4b3530cc9309286d81b689
Andrea Arcangeli <aarcange@redhat.com>
2010-11-02 21:46:57 +0100

    avoid breaking huge pmd invariants in case of vma_adjust failures
    An huge pmd can only be mapped if the corresponding 2M virtual range
    is fully contained in the vma. At times the VM calls split_vma twice,
    if the first split_vma succeeds and the second fail, the first
    split_vma remains in effect and it's not rolled back. For split_vma or
    vma_adjust to fail an allocation failure is needed so it's a very
    unlikely event (the out of memory killer would normally fire before
    any allocation failure is visible to kernel and userland and if an out
    of memory condition happens it's unlikely to happen exactly
    here). Nevertheless it's safer to ensure that no huge pmd can be left
    around if the vma is adjusted in a way that can't fit hugepages
    anymore at the new vm_start/vm_end address.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -106,6 +106,19 @@ extern void __split_huge_page_pmd(struct mm_struct *mm, pmd_t *pmd);
 
 extern unsigned long vma_address(struct page *page, struct vm_area_struct *vma);
 extern int hugepage_madvise(unsigned long *vm_flags);
+extern void __vma_adjust_trans_huge(struct vm_area_struct *vma,
+				    unsigned long start,
+				    unsigned long end,
+				    long adjust_next);
+static inline void vma_adjust_trans_huge(struct vm_area_struct *vma,
+					 unsigned long start,
+					 unsigned long end,
+					 long adjust_next)
+{
+	if (!vma->anon_vma || vma->vm_ops || vma->vm_file)
+		return;
+	__vma_adjust_trans_huge(vma, start, end, adjust_next);
+}
 static inline int PageTransHuge(struct page *page)
 {
 	VM_BUG_ON(PageTail(page));
@@ -137,6 +150,12 @@ static inline int hugepage_madvise(unsigned long *vm_flags)
 {
 	BUG_ON(0);
 	return 0;
+}
+static inline void vma_adjust_trans_huge(struct vm_area_struct *vma,
+					 unsigned long start,
+					 unsigned long end,
+					 long adjust_next)
+{
 }
 #endif /* CONFIG_TRANSPARENT_HUGEPAGE */
 
